eaaddf52eae30eded0eda5e06b2959cd315b23b6
henryjobel <jhobelsaha@gmail.com>
2023-10-20 02:51:20 +0600

    post created for the mongodb database

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ require('dotenv').config();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-
 //brandshop
 //jJSVYoyYnUqBeUNq
 
 //midware
 app.use(cors())
 app.use(express.json())
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.cmmcrj9.mongodb.net/?retryWrites=true&w=majority`;
@@ -29,16 +29,27 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+
+        const coffeeCollection = client.db('carsDB').collection('car');
+
+        app.post('/cars', async (req, res) => {
+            const newCars = req.body;
+            console.log(newCars);
+            const result = await coffeeCollection.insertOne(newCars);
+            res.send(result);
+        })
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
-
 
 
 
